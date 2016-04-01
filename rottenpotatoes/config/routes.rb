0d744ff081c37ffc2804d7ed9ec20c56b15fd208
burakkalac <burakkalac34@gmail.com>
2016-04-01 08:01:02 +0000

    homework4

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -13,6 +13,8 @@ Rottenpotatoes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
+  root :to => redirect('/movies')
+  get 'movies/:id/similar' => 'movies#similar', :as => :similar
 
   # Sample resource route with options:
   #   resources :products do
